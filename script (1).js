225e9d47f6faa4ea9a1299c1f45bc40cb8257ecf
rahulsingh83403-cpu <rahulsingh83403@gmail.com>
2025-12-27 02:13:29 +0530

    Add files via upload

---
+++ script (1).js
@@ -0,0 +1,19 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const observerOptions = {
+        root: null,
+        rootMargin: '0px',
+        threshold: 0.1
+    };
+
+    const observer = new IntersectionObserver((entries, observer) => {
+        entries.forEach(entry => {
+            if (entry.isIntersecting) {
+                entry.target.classList.add('show');
+                observer.unobserve(entry.target); // Only animate once
+            }
+        });
+    }, observerOptions);
+
+    const hiddenElements = document.querySelectorAll('.hidden');
+    hiddenElements.forEach((el) => observer.observe(el));
+});
